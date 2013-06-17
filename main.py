83bb1a060c85bfd0e22c0944f0e8407cdc9519d1
Timothy Davis <cpuobsessed@gmail.com>
2013-06-17 11:41:47 -0400

    first coding commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import game
+import menu
+import sys
+
+my_game = game.new()
+my_menu = menu.new()
+my_menu.exit_flag = False
+while (exit_flag == True):
+	my_menu.do_main()
+	
+sys.exit()
+
